6833c75ce492252587a04d33c18d6b67276ceec0
chetanchatrath <45016862+chetanchatrath@users.noreply.github.com>
2022-04-27 09:58:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features{}
   
-  subscription_id = "c614835e-9b30-4377-8676-159532c4a446"
-  tenant_id = "7a4d8448-3ab0-4c53-85f5-4f98b0e89d44"
-  client_id = "113aed03-3be9-4eb5-b55d-c01be05c4f76"
-  client_secret = "UnJ8Q~m5cZEQIJyHuw~FeLStZowqinXUJBGGnaqR"
+  subscription_id = "de5d5e82-d451-4b4a-8f92-5f2feeb1a318"
+  tenant_id = "72374233-443e-4c43-856d-6f9c86f0455a"
+  client_id = "19a4b959-99ac-4041-a747-b50b065bbf8b"
+  client_secret = "Anx8Q~.J_NHgblcNt0SOFVGvuOgykVgRa~GXqaeb"
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
